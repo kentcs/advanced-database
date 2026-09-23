2321385cf98ebc09883b1d657d9c7983ecad68bc
Greg DeLozier <gregdelozier@gmail.com>
2026-09-22 23:59:43 -0400

    Fix pet list rendering and ordering; remove unused template block

--- chapter-05-database-constraints/code/database.py
+++ chapter-05-database-constraints/code/database.py
@@ -78,7 +78,7 @@ def update_owner(id, data):
 
 def get_pets():
     cursor = connection.cursor()
-    cursor.execute("""select * from pet""")
+    cursor.execute("""select * from pet order by id""")
     pets = cursor.fetchall()
     pets = [dict(pet) for pet in pets]
     return pets
@@ -342,4 +342,3 @@ if __name__ == "__main__":
     test_delete_owner()
     close_connection()
     print("done.")
-
